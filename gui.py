a7024c455261cde4bd6732ed241e21c96b9bc6d4
mike <mike@Freedom>
2019-01-03 21:57:19 +0100

    changing GUI

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ def imprimir(texto): print ("text") # Imprime un texto
 root = Tk() # Creamos la raíz y se crea un bucle 
 root.title("Rock Paper Scissors Lizard Spock")
 root.geometry("800x600")
-root.resizable(1,1) # Como si fueran 0 = False y 1 = True
+root.resizable(0,0) # Como si fueran 0 = False y 1 = True
 menu = Menu(root)
 root.config(menu=menu)
 filemenu = Menu(menu)
@@ -26,17 +26,19 @@ n1 = StringVar()
 n2 = StringVar()
 
 #Main title
-label = Label(root, text="¡Otra etiqueta!")
+label = Label(root, text=text.get())
 label.pack(anchor="center")
 label.config(bg="white", fg="blue", font=("Arial",24))
-label.config(textvariable=text)
 
 #Output results
 textos = Text(root)
-textos.pack(side="top")
-textos.config(width=15, height=5, font=("Arial",15), padx=10, pady=5) # Aquí no son pixeles, si no caracteres
+textos.pack(anchor="center", side="top")
+textos.config(width=15, height=1, font=("Arial",20), padx=10, pady=5) # Aquí no son pixeles, si no caracteres
 
 #Buttons player 1
+label = Label(root, text="Player 1										Player2")
+label.pack(side="bottom")
+label.config(fg="blue", font=("Arial",12))
 Button(root, text="Rock", command=lambda: imprimir("Rock")).pack(side="left")
 Button(root, text="Paper", command=lambda: imprimir("Paper")).pack(side="left")
 Button(root, text="Scissors", command=lambda: imprimir("Scissors")).pack(side="left")
@@ -44,11 +46,11 @@ Button(root, text="Lizard", command=lambda: imprimir("Lizard")).pack(side="left"
 Button(root, text="Spock", command=lambda: imprimir("Spock")).pack(side="left")
 
 #Buttons player 2
-Button(root, text="Rock", command=lambda: imprimir("0")).pack(side="right")
-Button(root, text="Paper", command=lambda: imprimir("1")).pack(side="right")
-Button(root, text="Scissors", command=lambda: imprimir("2")).pack(side="right")
-Button(root, text="Lizard", command=lambda: imprimir("3")).pack(side="right")
-Button(root, text="Spock", command=lambda: imprimir("4")).pack(side="right")
+Button(root, text="Rock", command=lambda: imprimir("Rock")).pack(side="right")
+Button(root, text="Paper", command=lambda: imprimir("Paper")).pack(side="right")
+Button(root, text="Scissors", command=lambda: imprimir("Scissors")).pack(side="right")
+Button(root, text="Lizard", command=lambda: imprimir("Lizard")).pack(side="right")
+Button(root, text="Spock", command=lambda: imprimir("Spock")).pack(side="right")
 
 
 
